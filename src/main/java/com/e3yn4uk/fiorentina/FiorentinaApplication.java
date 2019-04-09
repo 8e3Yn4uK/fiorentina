56d3926e1e47f8120999f993d0c490a9faa1c914
8e3Yn4uK <sashko.kondratenko@gmail.com>
2019-04-09 11:38:04 +0200

    Initial commit

---
+++ src/main/java/com/e3yn4uk/fiorentina/FiorentinaApplication.java
@@ -0,0 +1,13 @@
+package com.e3yn4uk.fiorentina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiorentinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiorentinaApplication.class, args);
+	}
+
+}
